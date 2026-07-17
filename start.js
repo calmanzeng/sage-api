82766777aa66cfc80b4d700524b79a2a11e0e123
calmanzeng <123540001+calmanzeng@users.noreply.github.com>
2026-07-17 18:48:21 +0800

    Remove start.js

--- start.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify({ status: 'ok', message: 'Sage API Server' }));
-});
-const PORT = process.env.PORT || 3456;
-server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
